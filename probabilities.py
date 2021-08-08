8ad5f8c16e057d3a414a03e5b847a0a47d01f3a3
Andrew J Xu <ajx8@cornell.edu>
2021-08-07 22:03:28 -0700

    Added pot odds and fold frequency

--- probabilities.py
+++ probabilities.py
@@ -1,5 +1,11 @@
-def pot_odds(current_pot, bet):
-
+def pot_odds(pot, bet):
+    """Pot sized bet gives you 2:1 on your money, or 33%+ equity needed to continue."""
+    return (float(bet) + float(pot)) / (float(pot) + 2.0 * float(bet))
 
 def fold_frequency(pot, bet):
-    return float(bet)/(float(pot) + float(bet))
+    """Pot sized bet gives you a 50% fold frequency."""
+    return float(bet) / (float(pot) + float(bet))
+
+def should_call(equity, pot_odds):
+    """Basic function that uses only the expected value to calculate whether to call or not."""
+    return equity > pot_odds
